c375ad9805f8a9dcb0f8db48d04956fdf446f21a
aibunny <waihenyafredrick@gmail.com>
2023-12-14 16:05:42 +0300

    update

--- handler.py
+++ handler.py
@@ -29,17 +29,17 @@ def get_llm():
 
 
 def execute_prompt(question):
-    PROMPT = """ 
-    Given an input question, first look at the database then create a syntactically correct SQL query to run, 
-    Ensure the SQL query is correct and it matches the table names in the Microsoft Northwind database to avoid any errors before running. 
-    then look at the results of the query and return the answer in markdown format.If question requests explicitly requires
-    data in table format generate a markdown table with the data.  
-    Only use factual information from the Northwind database to answer the question if the question is not related to
-    Microsoft Northwind Database reply with "NOT RELATED"
-    NOTE: ALWAYS ENSURE SQL QUERY IS CORRECT. 
-    
-    The question: {question}
-    """
+    PROMPT = """
+        Given an input question, first look at the Microsoft Northwind Database then create a syntactically correct SQL query to run, 
+        Ensure the SQL query is correct and it matches the table names in the Microsoft Northwind database to avoid any errors before running. 
+        Then, look at the results of the query from the Microsoft Northwind Database and return the answer in markdown format.
+        If the question explicitly requires data in table format, generate a markdown table with the data from the Microsoft Northwind Database.  
+        Only use factual information from the Microsoft Northwind Database to answer the question. If the question is not related to
+        Microsoft Northwind Database, reply with "NOT RELATED."
+        NOTE: ALWAYS ENSURE SQL QUERY USES ONLY MICROSOFT NORTHWIND DATABASE DATA. 
+
+        The question: {question}
+        """
 
     db_chain = SQLDatabaseSequentialChain.from_llm(get_llm(),db)
 
