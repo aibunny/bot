b1441f0dfec301c1de41518816e107682b79c495
aibunny <waihenyafredrick@gmail.com>
2023-12-14 14:25:58 +0300

    update

---
+++ handler.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from langchain_experimental.sql import SQLDatabaseSequentialChain
+from langchain.utilities import SQLDatabase
+from langchain.llms import HuggingFaceHub
+from langchain.prompts import PromptTemplate
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+db = SQLDatabase.from_uri(DATABASE_URL)
+
+llm = HuggingFaceHub(
+        repo_id="HuggingFaceH4/zephyr-7b-beta",
+        model_kwargs={"max_length": 512}
+        )
+
+#question =" list top 5 customers along with their total orders in table format"
+
+def execute_prompt(question):
+    PROMPT = """ 
+    Given an input question, first create a syntactically correct SQL query to run,  
+    then look at the results of the query and return the answer in markdown format.If user requests explicitly requires
+    data in table format generate a markdown table with the data.  
+    The question: {question}
+    """
+
+    db_chain = SQLDatabaseSequentialChain.from_llm(llm,db)
+
+    result = db_chain.run(PROMPT.format(question=question))
+    print(result)
+    return result
+
+result = execute_prompt("list top 5 customers along with their total orders in table format")
+print(result)
--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from langchain.agents import create_sql_agent
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
-from langchain.agents.agent_types import AgentType
-from langchain.llms.openai import OpenAI
-from langchain.sql_database import SQLDatabase
 
+from handler import execute_prompt
 
 from dotenv import load_dotenv
 
@@ -33,24 +29,6 @@ class Message(BaseModel):
     content: str
 
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-db = SQLDatabase.from_uri(DATABASE_URL)
-
-toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI(temperature=0))
-
-# callback=FinalStreamingStdOutCallbackHandler()
-
-agent_executor = create_sql_agent(
-    llm=OpenAI(
-        streaming=True,
-        temperature=0
-    ),
-    toolkit=toolkit,
-    verbose=True,
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-)
-
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -59,5 +37,5 @@ async def read_root(request: Request):
 
 @app.post("/stream_chat/")
 async def stream_chat(message: Message):
-    result = agent_executor.run(message.content)
+    result = execute_prompt(message.content)
     return result
